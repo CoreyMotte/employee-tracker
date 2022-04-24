77d8580df23521e5b151cf9aa86da77226dcb75b
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-04-24 18:43:28 -0400

    added switch statement for options in menu

--- server.js
+++ server.js
@@ -13,4 +13,59 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("\n Employee Database Management System \n");
+    sysMenu();
 });
+
+function sysMenu() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "Please select the desired operation:",
+            choices: [
+                "View All Employees",
+                "View All Departments",
+                "View All Roles",
+                "Add Employee",
+                "Add Department",
+                "Add Role",
+                "Update Employee Role",
+                "Exit"
+            ]
+        })
+        .then(function(response) {
+            switch (response.action) {
+                case "View All Employees":
+
+                    break;
+
+                case "View All Departments":
+
+                    break;
+
+                case "View All Roles":
+
+                    break;
+
+                case "Add Employee":
+
+                    break;
+                
+                case "Add Department":
+
+                    break;
+
+                case "Add Role":
+
+                    break;
+                
+                case "Update Employee Role":
+
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        });
+};
