56931ced24b26ecfbe4681436eb6abc25168e88b
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-04-24 18:37:30 -0400

    added connection to server.js

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'employee_db'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("\n Employee Database Management System \n");
+});
